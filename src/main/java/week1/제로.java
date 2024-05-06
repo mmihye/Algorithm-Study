942db858be10f1a77a606a3c798cd84ff055ca18
mmihye <smilehye910@naver.com>
2024-05-06 19:04:53 +0900

    [BOJ]10773번 제로

---
+++ src/main/java/week1/제로.java
@@ -0,0 +1,34 @@
+package week1;
+
+import java.util.*;
+import java.io.*;
+
+public class 제로 {
+    public static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine(), "/n");
+
+        int k = Integer.parseInt(stk.nextToken());
+
+        Stack<Number> stack = new Stack<>();
+        for (int i = 0; i < k; i++) {
+            stk = new StringTokenizer(br.readLine(), "/n");
+            int n = Integer.parseInt(stk.nextToken());
+            if(n!=0)
+                stack.push(n);
+            else
+                stack.pop();
+        }
+
+        int sum = 0;
+        for(Number n : stack){
+            sum += n.intValue();
+        }
+
+        System.out.println(sum);
+
+
+    }
+
+}
